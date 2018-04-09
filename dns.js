ffc4b652de01798e3137b538d814280fb0825c91
Ken Smith <ken@onkea.com>
2018-04-08 22:59:15 -0700

    back to orig.

--- dns.js
+++ dns.js
@@ -6,15 +6,17 @@ var question = dns.Question({
   type: 'A',
 });
 
-
-request = dns.resolve('microsoft.com', function (err, results) {
-  results.forEach(function (result) {
-    console.log(result);
-  });
-});
-
 var start = Date.now();
 
+var req = dns.Request({
+  question: question,
+  server: { address: '8.8.8.8', port: 53, type: 'udp' },
+  timeout: 1000,
+});
+
+req.on('timeout', function () {
+  console.log('Timeout in making request');
+});
 
 req.on('message', function (err, answer) {
   answer.answer.forEach(function (a) {
